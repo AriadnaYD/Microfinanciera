1d524e7ccc65c238b409205dc0bda59390d162aa
Ariadna Dominguez <ariadnadominguez99@outlook.com>
2023-10-23 09:41:31 -0600

    avance 2.1.0

--- otorgarPrestamo.php
+++ otorgarPrestamo.php
@@ -9,7 +9,53 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="js/bootstrap.min.js"></script>
+    <style>
+        .mt-4 {
+            margin-top: 4px !important;
+        }
+        /* Estilos CSS para la barra de navegación */
+        .navbar {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #F5CAC3; /* Cambio de color del fondo de la barra de navegación */
+        }
 
+        .navbar li {
+            float: right;
+        }
+
+        .navbar li a {
+            display: block;
+            color: black;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Alineación a la derecha */
+        .navbar {
+            float: right;
+        }
+
+        /* Cambio de color del fondo donde va el título */
+        .menu-title {
+            background-color: #D9D9D9;
+        }
+
+        /* Cambio de color del fondo del navegador al hacer clic */
+        .navbar-nav .active > .nav-link {
+            background-color: blue !important;
+            color: white !important;
+        }
+
+        /* Ajuste del margen izquierdo del contenido principal */
+        .main-content {
+            margin-left: -15px; /* Ajusta el margen izquierdo para que coincida con el fondo del navbar */
+        }
+
+    </style>
 </head>
 
 <body>
@@ -25,9 +71,9 @@
         </div>
     </div>
     <!-- Barra de navegación-->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Menu</a>
+    <a class="navbar-brand" href="./vistas/menu.php">Menu</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -40,16 +86,15 @@
           <a class="nav-link" href="nuevoCliente.php">Nuevo Cliente</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="seguimientoPrestamo.php">Seguimiento de Préstamo</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="estadoCuenta.php">Estado de Cuenta</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-
 
     <!--BootStrap 5.3 JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
