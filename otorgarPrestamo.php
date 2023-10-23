43ec47d6ebcfa0397c502e6a1d17a90e46e7d9c0
Ariadna Dominguez <ariadnadominguez99@outlook.com>
2023-10-23 11:06:41 -0600

    2.5.0

--- otorgarPrestamo.php
+++ otorgarPrestamo.php
@@ -20,7 +20,7 @@
             margin: 0;
             padding: 0;
             overflow: hidden;
-            background-color: #F5CAC3; /* Cambio de color del fondo de la barra de navegación */
+            background-color: #D9D9D9; /* Cambio de color del fondo de la barra de navegación */
             float: right;
         }
 
@@ -38,7 +38,7 @@
 
         /* Cambio de color del fondo donde va el título */
         .menu-title {
-            background-color: #D9D9D9;
+            background-color: #D9D9D9; /* Cambio de color del fondo del título */
         }
 
         /* Cambio de color del fondo del navegador al hacer clic */
@@ -51,61 +51,58 @@
         .main-content {
             margin-left: -15px; /* Ajusta el margen izquierdo para que coincida con el fondo del navbar */
         }
-
     </style>
 </head>
 
 <body>
-    <div class="container-fluid p-0">
-        <!-- Barra de navegación -->
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #F5CAC3;">
-            <div class="container-fluid"> <!-- Cambiado a container-fluid para extender la barra -->
-                <a class="navbar-brand" href="./vistas/menu.php">Menu</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="otorgarPrestamo.php">Otorgar Prestamo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="nuevoCliente.php">Nuevo Cliente</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="seguimientoPrestamo.php">Seguimiento de Préstamo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="estadoCuenta.php">Estado de Cuenta</a>
-                        </li>
-                    </ul>
-                </div>
+    <div class="container-fluid p-0" style="background-color: #D9D9D9;">
+        <!-- Barra de navegación y logotipo -->
+        <div class="row" style="background-color: #D9D9D9;">
+            <div class="col-2" style="background-color: #D9D9D9;">
+                <!-- Mostrar el logotipo -->
+                <img src="./vistas/img/logo_micro.png" alt="Logo" class="w-100" style="height: 100px;">
             </div>
-        </nav>
-    </div>
-
-    <div class="row logo-section">
-        <div class="col-2">
-            <!-- Mostrar el logotipo -->
-            <img src="./vistas/img/logo_micro.png" alt="Logo" class="w-100" style="height: 50px;">
-        </div>
-        <div class="col-10 menu-title">
-            <!-- Mostrar el título del menú -->
-            <center><h1></h1></center>
+            <div class="col-10 menu-title">
+                <!-- Barra de navegación -->
+                <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #F5CAC3;">
+                    <div class="container"> <!-- Cambiado a container-fluid para extender la barra -->
+                        <a class="navbar-brand" href="./vistas/menu.php">Menu</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="otorgarPrestamo.php">Otorgar Prestamo</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="nuevoCliente.php">Nuevo Cliente</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="seguimientoPrestamo.php">Seguimiento de Préstamo</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="estadoCuenta.php">Estado de Cuenta</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
     </div>
 
     <!-- banner de la página -->
     <div class="container-fluid p-0">
-        <div class="row">
-            <div class="col-12 p-0">
+        <div class="row" >
+            <div class="col-12">
                 <!-- Mostrar el banner -->
                 <?php
                 // Ruta de la imagen del banner
                 $ruta_imagen = "./vistas/img/banner.png";
                 ?>
-                <img src="<?php echo $ruta_imagen; ?>" alt="Banner" class="w-100">
+                <img src="<?php echo $ruta_imagen; ?>" alt="Banner" class="w-100" style="height: 100px;">
             </div>
         </div>
     </div>
