93d95e56c6c9b60e714a7c32f7f6034ecdfb488f
Ariadna Dominguez <ariadnadominguez99@outlook.com>
2023-11-05 18:13:27 -0600

    5.1.1

--- administracion/validaregistro.php
+++ administracion/validaregistro.php
@@ -5,14 +5,14 @@
     $pass_cifrada = password_hash($pass_nocifrada, PASSWORD_DEFAULT, array("cost"=>10));
 
     if($conn == true){
-        $inserta = $conn -> prepare("INSERT INTO usuario(nombre_usuario,contraseña,aPaterno,aMaterno,id_direccion,id_correo,id_telefono) VALUES (:nombre_usuario, :contraseña, :aPaterno, :aMaterno, :id_direccion, :id_correo, :id_telefono)");
-        $inserta -> bindParam(':nombre_usuario', strtoupper($_POST['nombre']));
+        $inserta = $conn -> prepare("INSERT INTO usuario(correo,contraseña,nombre_usuario,aPaterno,aMaterno,direccion,telefono)VALUES (:correo, :contraseña, :nombre_usuario, :aPaterno, :aMaterno, :direccion, :telefono)");
+        $inserta -> bindParam(':correo', $_POST['usuario']);
         $inserta -> bindParam(':contraseña', $pass_cifrada);
+        $inserta -> bindParam(':nombre_usuario', strtoupper($_POST['nombre']));
         $inserta -> bindParam(':aPaterno', strtoupper($_POST['apaterno']));
         $inserta -> bindParam(':aMaterno', strtoupper($_POST['amaterno']));
-        $inserta -> bindParam(':id_direccion', $_POST['id_direccion']);
-        $inserta -> bindParam(':id_correo', $_POST['id_correo']);
-        $inserta -> bindParam(':id_telefono', $_POST['id_telefono']);
+        $inserta -> bindParam(':direccion', strtoupper($_POST['direccion']));
+        $inserta -> bindParam(':telefono', $_POST['telefono']);
         
         $inserta -> execute();
 
